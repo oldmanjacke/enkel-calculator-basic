4a6fc9b9a419896e2b55eb20697bf71c19df0d2b
jacob jolsö <jacke_j_1998@hotmail.se>
2019-10-27 22:20:22 +0100

    Add project files.

---
+++ enkel räknare/ConsolePrompt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace enkel_räknare
+{
+    internal class ConsolePrompt
+    {
+        private object p;
+
+        public ConsolePrompt(object p)
+        {
+            this.p = p;
+        }
+
+        internal object Prompt<T>(object p1, object required, object p2, object p3, Func<object, bool> p4)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ enkel räknare/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace enkel_räknare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+                double firstNum;
+                double secondNum;                   //Variables for equation
+                string operation;
+                double answer;
+
+                Console.WriteLine("Hello, welcome  basic calculator!");
+                Console.ReadLine();
+
+                Console.Write("Enter the first number in your basic equation: ");
+                firstNum = Convert.ToInt32(Console.ReadLine());
+
+                //User input for equation
+                Console.Write("Now enter your second number in the basic equation: ");
+                secondNum = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Ok now enter your operation ( x , / , +, -) ");
+                operation = Console.ReadLine();
+
+                if (operation == "x")
+                {
+                    answer = firstNum * secondNum;
+                    Console.WriteLine(firstNum + " x " + secondNum + " = " + answer);
+                    Console.ReadLine();
+                }
+                else if (operation == "/")
+                {
+                    answer = firstNum / secondNum;
+                    Console.WriteLine(firstNum + " / " + secondNum + " = " + answer);
+                    Console.ReadLine();
+                }
+                //Getting answers
+                else if (operation == "+")
+                {
+                    answer = firstNum + secondNum;
+                    Console.WriteLine(firstNum + " + " + secondNum + " = " + answer);
+                    Console.ReadLine();
+                }
+                else if (operation == "-")
+                {
+                    answer = firstNum - secondNum;
+                    Console.WriteLine(firstNum + " - " + secondNum + " = " + answer);
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Sorry that is not correct format! Please restart!");     //Catch
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+
+}
+    
+
