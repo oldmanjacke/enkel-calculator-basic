56b6b78a669dfe1f993f1c35ab5732b0b37ecc52
jacob jolsö <jacke_j_1998@hotmail.se>
2019-10-27 23:08:20 +0100

    lagt till mer kod som ska leda till bättre utseende och stabilitet

--- enkel räknare/Program.cs
+++ enkel räknare/Program.cs
@@ -4,14 +4,24 @@ namespace enkel_räknare
 {
     class Program
     {
+       
+     
         static void Main(string[] args)
         {
-
+            
             {
                 double firstNum;
                 double secondNum;                   //Variables for equation
                 string operation;
                 double answer;
+                
+                var prompter = new ConsolePrompt(null);
+                var valdidoperations = new[] { "x", "/", "+", "-" };
+                var firstNum = prompter.Prompt<int>("enter first number", PromptOptions.Required);
+                var secoundNum = prompter.Prompt<int>("enter your operation", PromptOptions.Required);
+                var operation = prompter.Prompt<string>("enter your secound number("+string.Join(",", valdidoperations) +")", PromptOptions.Required, validationMethod: x => valdidoperations.Contains(x));
+
+
 
                 Console.WriteLine("Hello, welcome  basic calculator!");
                 Console.ReadLine();
@@ -21,39 +31,45 @@ namespace enkel_räknare
 
                 //User input for equation
                 Console.Write("Now enter your second number in the basic equation: ");
-                secondNum = Convert.ToInt32(Console.ReadLine());
+                secondNum = Convert.ToInt32(Console.ReadLine()); //något fel här med input
                 Console.Write("Ok now enter your operation ( x , / , +, -) ");
                 operation = Console.ReadLine();
 
-                if (operation == "x")
+                switch (operation)
                 {
+
+                
+                
+                    case "x":
                     answer = firstNum * secondNum;
-                    Console.WriteLine(firstNum + " x " + secondNum + " = " + answer);
-                    Console.ReadLine();
-                }
-                else if (operation == "/")
-                {
+                   
+                        break; 
+                
+                   case "/":
+                
                     answer = firstNum / secondNum;
-                    Console.WriteLine(firstNum + " / " + secondNum + " = " + answer);
-                    Console.ReadLine();
-                }
-                //Getting answers
-                else if (operation == "+")
-                {
+                    
+                    break;
+
+                    //tanken är att personen ska skriva in sina siffror och ska det sen i slut ändan visas som typ 5x2=10
+                    case "+":
+                
                     answer = firstNum + secondNum;
-                    Console.WriteLine(firstNum + " + " + secondNum + " = " + answer);
-                    Console.ReadLine();
-                }
-                else if (operation == "-")
-                {
+                    
+                 break;
+
+                    case "-":
+                
                     answer = firstNum - secondNum;
-                    Console.WriteLine(firstNum + " - " + secondNum + " = " + answer);
-                    Console.ReadLine();
-                }
-                else
-                {
+                    
+                    break; 
+                
+                  
+                
+                
                     Console.WriteLine("Sorry that is not correct format! Please restart!");     //Catch
                     Console.ReadLine();
+                        
                 }
             }
         }
